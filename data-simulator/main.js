f20435e759de20fee02ca52a5957675418131666
Christoph Wen <christoph.wen@tum.de>
2020-04-17 16:22:52 +0200

    simple print out server working

--- data-simulator/main.js
+++ data-simulator/main.js
@@ -1,6 +1,18 @@
-const io = require("socket.io-client")
-const ioClient = io.connect("http://localhost:8000")
+const io = require("socket.io-client");
+const ioClient = io.connect("http://localhost:8000");
 
+var intervalHandle = null;
 
+ioClient.on('connect', (socket) => {
+    intervalHandle = setInterval(function(){ myFnc(); }, 3000);
+});
+
+ioClient.on('disconnect', (socket) => {
+    clearInterval(intervalHandle)
+});
 
 console.log("Test message");
+
+function myFnc() {
+    ioClient.emit('msg', 'test');
+}
--- data-simulator/print-out-server.js
+++ data-simulator/print-out-server.js
@@ -3,14 +3,19 @@ const io = require("socket.io")
 const server = io.listen(8000)
 
 server.on('connection', (socket) => {
-    console.info(`Client connected [id=${socket.id}]`)
+    console.info(`Client connected [id=${socket.id}]`);
 
-
-    socket.on('sendMessage', (message) => {
-        console.log(message)
-    });
+    socket.on('msg', (msg) => {
+        console.log(msg)
+    })
 
     socket.on("disconnect", () => {
-        console.info(`Client disconnected [id=${socket.id}]`)
+        console.info(`Client disconnected [id=${socket.id}]`);
     });
+});
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    server.close();
+    process.exit();
 });
