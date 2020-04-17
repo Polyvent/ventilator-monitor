f1cd77c648794a2b679f4e65e560a6d169baa111
Bernhard Vorhofer <b4hofer@gmail.com>
2020-04-17 20:43:37 +0200

    Added vitalsigns to database code

--- ventilator-server/server.js
+++ ventilator-server/server.js
@@ -12,41 +12,7 @@ const handle    = nextApp.getRequestHandler()
 
 const PORT = 8080
 
-var db = new sqlite3.Database('ventDB');
-
-db.serialize(function() {
-    db.run("CREATE TABLE if not exists ventilators (deviceID int, firstName varchar(100), lastName varchar(100))")
-    db.run(
-        `CREATE TABLE if not exists dataset 
-           (deviceID int,
-            expiredCO2 int,
-            expiredO2 int,
-            MVe int,
-            frequency int,
-            FiO2 int,
-            IE float,
-            IVe int,
-            PEEP float,
-            RR int,
-            VT int,
-            humidity int,
-            pressureMax int,
-            pressureMean int,
-            ventilationMode string,
-            volumePerMinute int,
-            volumePerMovement int,
-            CO2 int,
-            O2 int,
-            angleSensor int,
-            current int,
-            motorRPM int,
-            pressure1 int,
-            pressure2 int,
-            temperature1 int,
-            temperature2  int,
-            time bigint)`
-        )
-})
+var db = new sqlite3.Database('database.db');
 
 nextApp.prepare()
 .then(() => {
@@ -67,35 +33,40 @@ nextApp.prepare()
             // add data to DB
             data.forEach(dataPoint =>
                 db.serialize(function () {
-                    var stmt = db.prepare("INSERT INTO dataset VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+                    var stmt = db.prepare("INSERT INTO dataset VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
                     stmt.run(
-                        dataPoint.device_id,
-                        dataPoint.processed.ExpiredCO2,
-                        dataPoint.processed.ExpiredO2,
-                        dataPoint.processed.MVe,
-                        dataPoint.processed.frequency,
-                        dataPoint.processed.triggerSettings.FiO2,
-                        dataPoint.processed.triggerSettings.IE,
-                        dataPoint.processed.triggerSettings.MVe,
-                        dataPoint.processed.triggerSettings.PEEP,
-                        dataPoint.processed.triggerSettings.RR,
-                        dataPoint.processed.triggerSettings.VT,
-                        dataPoint.processed.triggerSettings.humidity,
-                        dataPoint.processed.triggerSettings.pressure_max,
-                        dataPoint.processed.triggerSettings.pressure_mean,
-                        dataPoint.processed.ventilationMode,
-                        dataPoint.processed.volumePerMinute,
-                        dataPoint.processed.volumePerMovement,
-                        dataPoint.raw.CO2,
-                        dataPoint.raw.O2,
-                        dataPoint.raw.angleSensor,
-                        dataPoint.raw.current,
-                        dataPoint.raw.motorRPM,
-                        dataPoint.raw.pressure1,
-                        dataPoint.raw.pressure2,
-                        dataPoint.raw.temperature1,
-                        dataPoint.raw.temperature2,
-                        dataPoint.time)
+                        dataPoint.ventdata.device_id,
+                        dataPoint.ventdata.processed.ExpiredCO2,
+                        dataPoint.ventdata.processed.ExpiredO2,
+                        dataPoint.ventdata.processed.MVe,
+                        dataPoint.ventdata.processed.frequency,
+                        dataPoint.ventdata.processed.triggerSettings.FiO2,
+                        dataPoint.ventdata.processed.triggerSettings.IE,
+                        dataPoint.ventdata.processed.triggerSettings.MVe,
+                        dataPoint.ventdata.processed.triggerSettings.PEEP,
+                        dataPoint.ventdata.processed.triggerSettings.RR,
+                        dataPoint.ventdata.processed.triggerSettings.VT,
+                        dataPoint.ventdata.processed.triggerSettings.humidity,
+                        dataPoint.ventdata.processed.triggerSettings.pressure_max,
+                        dataPoint.ventdata.processed.triggerSettings.pressure_mean,
+                        dataPoint.ventdata.processed.ventilationMode,
+                        dataPoint.ventdata.processed.volumePerMinute,
+                        dataPoint.ventdata.processed.volumePerMovement,
+                        dataPoint.ventdata.raw.CO2,
+                        dataPoint.ventdata.raw.O2,
+                        dataPoint.ventdata.raw.angleSensor,
+                        dataPoint.ventdata.raw.current,
+                        dataPoint.ventdata.raw.motorRPM,
+                        dataPoint.ventdata.raw.pressure1,
+                        dataPoint.ventdata.raw.pressure2,
+                        dataPoint.ventdata.raw.temperature1,
+                        dataPoint.ventdata.raw.temperature2,
+                        dataPoint.vitalsigns.bloodpressure.systole,
+                        dataPoint.vitalsigns.bloodpressure.diastole,
+                        dataPoint.vitalsigns.bodyTemperature,
+                        dataPoint.vitalsigns.heartRate,
+                        dataPoint.vitalsigns.oxygenSaturation,
+                        dataPoint.ventdata.time)
             
                     stmt.finalize()
                 })
@@ -123,10 +94,52 @@ nextApp.prepare()
         
     })
 
-    // Start HTTP server
-    http.listen(PORT, (err) => {
-        if (err) throw err
-        console.log("Listening on port " + PORT)
-    })
+    db.serialize(function() {
+        console.log('Setting up database...')
+        db.run("CREATE TABLE if not exists ventilators (deviceID int, firstName varchar(100), lastName varchar(100))")
+        db.run(
+            `CREATE TABLE if not exists dataset 
+               (deviceID int,
+                expiredCO2 int,
+                expiredO2 int,
+                MVe int,
+                frequency int,
+                FiO2 int,
+                IE float,
+                IVe int,
+                PEEP float,
+                RR int,
+                VT int,
+                humidity int,
+                pressureMax int,
+                pressureMean int,
+                ventilationMode string,
+                volumePerMinute int,
+                volumePerMovement int,
+                CO2 int,
+                O2 int,
+                angleSensor int,
+                current int,
+                motorRPM int,
+                pressure1 int,
+                pressure2 int,
+                temperature1 int,
+                temperature2  int,
+                bloodPressureSystole int,
+                bloodPressureDiastole int,
+                bodyTemperature int,
+                heartRate int,
+                oxygenSaturation int,
+                time bigint)`
+            )
+        db.parallelize(() => {
+            console.log('Database setup successful. Starting HTTP server...')
 
+            // Start HTTP server after database initialization
+            http.listen(PORT, (err) => {
+                if (err) throw err
+                console.log("Listening on port " + PORT)
+            })
+        })
+    })
 })
