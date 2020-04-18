75f69a48244d1ddd321be49c9d02a89c108aa2fa
Sébastien Letzelter <sebastien.letzelter@gmail.com>
2020-04-18 21:40:11 +0200

    Added triggers

--- ventilator-server/components/Settings.js
+++ ventilator-server/components/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import SettingsBackground from './SettingsBackground';
-
+import TriggerInput from './TriggerInput';
 
 export default class Settings extends React.Component {
     constructor(props) {
@@ -15,14 +15,18 @@ export default class Settings extends React.Component {
 
                 <div className="settings-pop-up">
                     <h2>Settings</h2>
-
+                    <p>Patient</p>
                     <div>
                         <label for="fname">First name:</label>
                         <input type="text" id="fname" name="fname"></input>
                         <label for="lname">Last name:</label>
                         <input type="text" id="lname" name="lname"></input>
-                        <input type="button" value="LOLLO" />
                     </div>
+                    <p>Triggers</p>
+                    <div>
+                        <TriggerInput />
+                    </div>
+                    <input type="button" value="Update" />
                 </div>
             </div>
         );
