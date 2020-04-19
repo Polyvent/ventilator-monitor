8915ccb6893b62bcd9c573d692020622015f369a
Sébastien Letzelter <sebastien.letzelter@gmail.com>
2020-04-19 02:53:37 +0200

    elem moving of 1px

--- ventilator-server/components/TriggerInput.js
+++ ventilator-server/components/TriggerInput.js
@@ -13,15 +13,16 @@ export default class TriggerInput extends React.Component {
                 <div className="trigger-min">
                     <label for={"min"+this.props.name}>Min </label>
                     <p className="set-button set-minus"> <span>-</span></p>
-                    <input type="text" name={"min"+this.props.name} placeholder="123" />
-                    <p className="settings-unit">{this.props.unit}</p>
+                    <span className="settings-input"><input type="text" name={"min"+this.props.name} placeholder="123" />
+                    <input className="settings-unit" type="text" value={this.props.unit} disabled/></span>
+
                     <p className="set-button"> <span>+</span></p>
                 </div>
                 <div className="trigger-max">
                     <label for={"max"+this.props.name}>Max </label>
                     <p className="set-button set-minus"> <span>-</span></p>
-                    <input type="text" name={"max"+this.props.name} placeholder="123" />
-                    <p className="settings-unit">{this.props.unit}</p>
+                    <span class="settings-input"><input type="text" name={"max"+this.props.name} placeholder="123" />
+                    <input className="settings-unit" type="text" value={this.props.unit} disabled/></span>
                     <p className="set-button"> <span>+</span></p>
                 </div>
             </div>
